b4287401f5ae629f852327b81daec517f8db8906
aaravhajare <hajareaarav66@gmail.com>
2025-02-02 12:37:38 +0530

    typecasting

---
+++ Typecasting.py
@@ -0,0 +1,31 @@
+# intilizing variable
+
+name = "Aarav"
+Age = 11
+Weight = 35.5
+student = True
+
+#print variable
+
+print("My name is" , name)
+print("Data Type is" , type(name))
+
+print("My age is" , Age)
+print("Data Type is" , type(Age))
+
+print("My weight is" , Weight)
+print("Data Type is" , type(Weight))
+
+print("I am a student" , student)
+print("Data Type is" , type(student))
+
+# type casting
+
+Age = str(Age)
+print(Age)
+print("Data Type is" , type(Age))
+
+
+Weight = int(Weight)
+print(Weight)
+print("Data Type is" , type(Weight))
