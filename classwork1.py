43353e53919657a3735d3bd91b2b9452a3953267
aaravhajare <hajareaarav66@gmail.com>
2025-02-02 12:27:48 +0530

    classwork1

---
+++ classwork1.py
@@ -0,0 +1,14 @@
+#to identitfy type variable
+
+a = 2
+print("type of variable :" , type(a))
+
+b = 3.5
+print("type of variable :" , type(b))
+
+c = "Aarav"
+print("type of variable :" , type(c))
+
+d = True
+print("type of variable :" , type(d))
+
