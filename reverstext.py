d8e66969b45532865f50384ce804c8d1de6d312e
aaravhajare <hajareaarav66@gmail.com>
2025-02-02 12:46:22 +0530

    reverse text

---
+++ reverstext.py
@@ -0,0 +1,19 @@
+
+# intializing varialbe
+
+text = str(input("enter a number"))
+
+print(text)
+
+# reversing text
+
+revtext = text[::-1]
+
+text = revtext
+
+print(revtext)
+print("Data Type is" , type(text))
+
+text = int(text)
+print("Data Type is" , type(text))
+
