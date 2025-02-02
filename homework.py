a1d28ffad8456f6cb5874a2674f53e42e6a4c455
aaravhajare <hajareaarav66@gmail.com>
2025-02-02 12:53:02 +0530

    homework

---
+++ homework.py
@@ -0,0 +1,16 @@
+
+# variable
+
+a = "AaRav"
+
+print(a)
+print(str.lower(a))
+print(str.upper(a))
+
+str1 = input("Enter a string")
+str2 = input("Enter a string")
+
+str3 = str1 + " " + str2
+print(str3)
+
+print("Data Type is" , type(str3))
